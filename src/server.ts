824a743062270ccaf15059d03c6b0ef3d2584f96
verve-neowise <verve.neowise@gmail.com>
2022-10-06 14:38:57 +0500

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello CI/CD')
+})
+
+app.listen(8080, () => {
+    console.log(`Server running on http://localhost:${8080}`)
+})
